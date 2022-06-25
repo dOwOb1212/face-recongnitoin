e74ae942a1cb04cbaee5e4ef90924c1db26f88bf
dOwOb1212 <augusthsuangwei@gmail.com>
2022-06-26 00:12:02 +0800

    6/26

--- face recongnition with python/facial_verification_with_Siamese_Network.py
+++ face recongnition with python/facial_verification_with_Siamese_Network.py
@@ -2,5 +2,35 @@ import cv2
 import os
 import random
 import numpy as np
+
 from matplotlib import pyplot as plt
+from tensorflow.keras.models import  Model
+from tensorflow.keras.layers import Layer, Conv2D, Dense, MaxPooling2D, Input, Flatten
+import tensorflow as tf
+
+def gpu_growth():
+    # Avoid OOM errors by setting GPU Memory Consumption Growth
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu,True)
+
+def create_folder_structure():
+
+    # Setup paths
+    POS_PATH = os.path.join('data', 'positive')
+    NEG_PATH = os.path.join('data', 'negative')
+    ANC_PATH = os.path.join('data', 'anchor')
+    
+    # Make the directories
+    os.makedirs(POS_PATH)
+    os.makedirs(NEG_PATH)
+    os.makedirs(ANC_PATH)
+
+    # Move Images to the following repository data/negative    
+    for directory in os.listdir('lfw'):
+        for file in os.listdir(os.path.join('lfw', directory)):
+            EX_PATH = os.path.join('lfw', directory, file)
+            NEW_PATH = os.path.join(NEG_PATH, file)
+            os.replace(EX_PATH, NEW_PATH)
+
 
